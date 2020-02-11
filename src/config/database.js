674d2117dfe3082c73a0523df1981bf63dc673b3
Assumps <jrvitor5@gmail.com>
2020-02-10 23:36:56 -0300

    erro criação de migrate

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
+  password: 'dockers',
   database: 'gobarber',
   define: {
     timestamps: true,
