a251949223d3bb6635eab29ddb383f189d9fd4e3
Assumps <jrvitor5@gmail.com>
2020-02-24 02:20:15 -0300

    validação de dados de entrada com yup e criação de metodo update

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
 import Usercontroller from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
 
 routes.post('/users', Usercontroller.store);
+routes.post('/sessions', SessionController.store);
+routes.use(authMiddleware);
+routes.put('/users', Usercontroller.update);
 
 export default routes;
