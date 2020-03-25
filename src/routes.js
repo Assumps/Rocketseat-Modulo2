96ce2cb39a9ed34294298248fc7cb701c3554f4a
Assumps <jrvitor5@gmail.com>
2020-03-25 18:36:19 -0300

    Inserção de Upload de Arquivos e listagem de prestadores de serviço

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,20 @@
 import { Router } from 'express';
-
+import multer from 'multer';
+import multerConfig from './config/multer';
 import Usercontroller from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', Usercontroller.store);
 routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', Usercontroller.update);
+routes.get('/providers', ProviderController.index);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
